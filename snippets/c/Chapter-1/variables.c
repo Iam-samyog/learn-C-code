b3120c71ed9e4d225a4cf6a801885d20dff7d12a
Iam-samyog <msamyog37@gmail.com>
2025-04-20 15:49:41 +0545

    Added Variables and Chapter-1 to the git

---
+++ snippets/c/Chapter-1/variables.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+//Code displays how data types work in the C
+/*Comment is really usfull because it helps programmer to give information of the program to other programmers.*/
+
+
+int main(){
+    int age=22;
+    float pi=3.14;
+    char hashtag='#';
+    
+
+    printf("C-language\t");
+    printf("Samyog Maharjan\n");
+    printf("C-language");
+
+    printf("Age is %d",age);
+    return 0;
+}
