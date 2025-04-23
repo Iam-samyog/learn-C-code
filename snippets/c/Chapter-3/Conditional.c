1e27ed0bd69dc0fcbe861cbe3b45d470d6ecdb21
Iam-samyog <msamyog37@gmail.com>
2025-04-23 15:21:51 +0545

    Added Chapter-3 to the folder

---
+++ snippets/c/Chapter-3/Conditional.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int age=19;
+    printf("Enter age :");
+    scanf("%d",&age);
+
+    if(age >=18){
+        printf("You can vote.\n");
+
+    }
+    else{
+        printf("You cannot vote.\n")
+    }
+}
