d22c55dacf153fd28b56ce3c5dedfcf9ae58394b
Iam-samyog <msamyog37@gmail.com>
2025-04-23 14:52:07 +0545

    Added qn3.c

---
+++ snippets/c/Chapter-2/qn3.c
@@ -0,0 +1,9 @@
+//Write a program to print the average of 4 numbers
+
+#include<stdio.h>
+int main(){
+    float a=4,b=5,c=10,d=100;
+    printf("Average of fours numbers is %.2f\n",(a+b+c+d)/4);
+
+    return 0;
+}
