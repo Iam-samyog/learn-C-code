b4550573fe4944c5ce40482524e42fd3e633b356
Iam-samyog <msamyog37@gmail.com>
2025-05-03 20:44:27 +0545

    Added Function and qn1.c

---
+++ snippets/c/Chapter-5/Function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//Function Declaration
+void printHello();
+
+int main(){
+    printHello(); // Function Call
+  
+    return 0;
+}
+void printHello(){
+    printf("Hello World!\n");// Function Definition
+}
---
+++ snippets/c/Chapter-5/qn1.c
@@ -0,0 +1,27 @@
+//Write 3 functions - one to print Hello, You Name and goodbye
+
+#include<stdio.h>
+
+void printHello();
+void printName();
+void printBye();
+
+int main(){
+    printHello();
+    printName();
+    printBye();    
+
+    return 0;
+}
+
+void printHello(){
+    printf("Hello!\n");
+}
+void printName(){
+    
+    printf("My Name is Samyog Maharjan\n");
+   
+}
+void printBye(){
+    printf("GoodBye!");
+}
