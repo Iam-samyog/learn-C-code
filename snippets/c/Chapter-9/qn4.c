393c7bb6202d207d51aa1564ed787a821cbffa69
Iam-samyog <msamyog37@gmail.com>
2025-05-11 21:55:15 +0545

    Added qn4.c

---
+++ snippets/c/Chapter-9/qn4.c
@@ -0,0 +1,24 @@
+//Create a structure to store complex number use arrow operator
+
+#include<stdio.h>
+struct complex{
+    int r;
+    int i;
+};
+void storeComplex(struct complex c1);
+int main(){
+    struct complex c1;
+
+    printf("Enter your real part of the number :");
+    scanf("%d",&c1.r);
+
+    printf("Enter your img part of the number :");
+    scanf("%d",&c1.i);
+    storeComplex(c1);
+
+
+}
+void storeComplex(struct complex c1){
+    struct complex *ptr=&c1;
+    printf("The complex number you have stored is %d+%di",ptr->r,ptr->i);
+}
